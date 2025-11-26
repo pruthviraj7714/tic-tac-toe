17bae2bb91da95379cea45be45635849f77b2a04
pruthviraj7714 <marganepruthviraj@gmail.com>
2025-11-26 23:17:52 +0530

    Backend + DB Init

---
+++ apps/backend/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import userRouter from "./routes/user";
+import roomRouter from "./routes/room";
+import gameRouter from "./routes/game";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Healthy Server",
+  });
+});
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/room", roomRouter);
+app.use("/api/v1/game", gameRouter);
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
