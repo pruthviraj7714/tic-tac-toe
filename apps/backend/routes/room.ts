e7eadba18fbd76937f9cb2bc60ee3445d14dea45
pruthviraj7714 <marganepruthviraj@gmail.com>
2025-11-28 23:56:14 +0530

    Added basic user auth route signatures

--- apps/backend/routes/room.ts
+++ apps/backend/routes/room.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-
 const roomRouter = Router();
 
 export default roomRouter;
--- apps/backend/routes/user.ts
+++ apps/backend/routes/user.ts
@@ -1,6 +1,15 @@
 import { Router } from "express";
 
-
 const userRouter = Router();
+
+userRouter.post('/signup', async (req, res) => {
+
+})
+
+
+userRouter.post('/signin', async (req, res) => {
+    
+})
+
 
 export default userRouter;
